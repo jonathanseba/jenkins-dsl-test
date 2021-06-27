bbfe00d460840c7cdaa347a5f374eb69159e3f07
jonathanseba <73753398+jonathanseba@users.noreply.github.com>
2021-06-27 22:46:45 +0900

    Update nodejs.groovy

--- nodejs.groovy
+++ nodejs.groovy
@@ -13,14 +13,6 @@ job('NodeJS example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
-            tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
-            forcePull(false)
-            forceTag(false)
-            createFingerprints(false)
-            skipDecorate()
-        }
+        shell("npm install")
     }
 }
